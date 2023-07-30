bbc2593bf897ea83fc9459c0c3580405d6f24491
johanyim <johanjyyim@gmail.com>
2023-07-30 14:35:27 +0100

    added slider to adjust dimensions of the grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,20 +3,30 @@ let currentColor = BLACK
 let dim = 10;
 
 const grid = document.getElementById("grid")
+const slider = document.getElementById("dimension");
 
+function makeGrid(grid, dim){
+    //delete all elements in grid class
+    grid.innerHTML = '';
 
-for(let i = 0; i < dim; i++){
-    let row = document.createElement("div")
-    row.classList.add("row")
-    for(let j = 0; j < dim; j++){
-        let square = document.createElement("div")
-        square.classList.add("square")
-        row.appendChild(square)
+    for(let i = 0; i < dim; i++){
+        let row = document.createElement("div")
+        row.classList.add("row")
+        for(let j = 0; j < dim; j++){
+            let square = document.createElement("div")
+            square.classList.add("square")
+            row.appendChild(square)
+        }
+        grid.appendChild(row)
     }
-    grid.appendChild(row)
+}
+
+slider.oninput = function() {
+    makeGrid(grid, this.value);
 }
 
 
 
+makeGrid(grid, dim);
 
-console.log(grid)
+
